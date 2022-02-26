8b028e976680058acedc046bea5b545401f7cb2a
Miguel Angel Camacho <miguel.camacho@gradiweb.com>
2022-02-25 19:49:36 -0500

    fixesstyle services

--- src/components/HomePage/Services.js
+++ src/components/HomePage/Services.js
@@ -8,17 +8,9 @@ import React from 'react';
 const Services = () => {
     
     return (
-        <div className="Services row col-12 ">
-            <div  className="ServicesImg row col-12 col-lg-8 col-xl-6 mx-auto">
-                {/* <div className="ServicesImg1">
-                    <LocalShippingIcon style={{color: '#0096d2', fontSize: 40}}/>
-                    <h6>Envio Gratis</h6>
-                </div> */}
-                {/* <div className="ServicesImg2">
-                    <PhoneIcon style={{color: '#0096d2', fontSize: 40}}/>
-                    <h6>Soporte 24/7</h6>
-                </div> */}
-                <div className="ServicesImg3 col-10 col-sm-6 col-md-5 mb-3">
+        <div className="Services row col-12 mx-auto">
+            <div  className="ServicesImg row col-12 col-lg-8 col-xl-6 mx-auto p-0 ">
+                <div className="ServicesImg3 col-10 col-sm-6 col-md-5 ">
                     <PaymentIcon style={{color: '#0096d2', fontSize: 40}}/>
                     <h6>Pagos seguros</h6>
                 </div>
